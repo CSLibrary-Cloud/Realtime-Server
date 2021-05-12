4b119866e2e10a010b17d211770b329a2a84e42d
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-05-13 00:50:53 +0900

    stomp_server

--- stomp_server/src/main/kotlin/com/example/stomp_server/StompServerApplication.kt
+++ stomp_server/src/main/kotlin/com/example/stomp_server/StompServerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ServerStompApplication
+class StompServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<ServerStompApplication>(*args)
+    runApplication<StompServerApplication>(*args)
 }
