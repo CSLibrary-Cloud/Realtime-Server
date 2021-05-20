f9a1598908a506603756c6d6024917bdf36075e0
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-05-21 04:15:28 +0900

    ktor 활용 실시간 서버

---
+++ Realtime_Server/src/test/kotlin/com/example/Realtime_Server/RealtimeServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Realtime_Server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealtimeServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
